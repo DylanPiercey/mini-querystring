0a1114aec259289027431cb66ddd7574905ea730
Dylan Piercey <pierceydylan@gmail.com>
2017-10-16 16:20:45 -0700

    * Improve jsdoc.

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,16 @@ import { deep as setDeep, shallow as setShallow } from "q-set";
 const parseReg: RegExp = /([^=?&]+)=?([^&]*)/g;
 
 /**
+ * @description
  * Converts an object to a query string and optionally flattens it.
  *
  * @example
  * stringify({ a: 1 }) === 'a=1'
+ *
  * stringify({ a: { b: 1 } }, true) === 'a[b]=1'
+ *
+ * @param obj The object to stringify.
+ * @param deep If true the object will be flattened using query string syntax.
  */
 export function stringify(obj: any, deep?: boolean): string {
   if (deep) {
@@ -28,11 +33,16 @@ export function stringify(obj: any, deep?: boolean): string {
 }
 
 /**
+ * @description
  * Parses a query string and optionally unflattens it.
  *
  * @example
  * parse('a=1&b=2&') === "{ a: '1', b: '2' }"
+ *
  * parse('a=1&b[c]=2', true) === "{ a: '1', b: { c: '1' } }"
+ *
+ * @param str The string to parse.
+ * @param deep If true, nested querystring paths will be resolved.
  */
 export function parse(str: string, deep?: boolean): any {
   const set = deep ? setDeep : setShallow;
